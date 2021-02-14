30672e707fc0d0cf43017b8ff047bb2a15d98c09
haseebm139 <haseebm139@gmail.com>
2021-02-14 19:53:58 +0500

    Real Time database complete>>>>>

---
+++ app/src/main/java/com/example/final_project/UserHelperClass.java
@@ -0,0 +1,66 @@
+package com.example.final_project;
+
+public class UserHelperClass {
+    String fname, lname, email, phoneNo, dOb, pasword;
+
+    public UserHelperClass() {
+
+    }
+
+    public UserHelperClass(String fname, String lname, String email, String phoneNo, String password) {
+        this.fname = fname;
+        this.lname = lname;
+        this.email = email;
+        this.phoneNo = phoneNo;
+        this.dOb = dOb;
+        this.pasword = pasword;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getdOb() {
+        return dOb;
+    }
+
+    public void setdOb(String dOb) {
+        this.dOb = dOb;
+    }
+
+    public String getPasword() {
+        return pasword;
+    }
+
+    public void setPasword(String pasword) {
+        this.pasword = pasword;
+    }
+}
