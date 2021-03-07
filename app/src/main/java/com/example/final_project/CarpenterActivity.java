457fc45c628217276d3671c911063180bfb70cf5
haseebm139 <haseebm139@gmail.com>
2021-03-08 01:54:57 +0500

    Provider Data

--- app/src/main/java/com/example/final_project/CarpenterActivity.java
+++ app/src/main/java/com/example/final_project/CarpenterActivity.java
@@ -28,18 +28,18 @@ public class CarpenterActivity extends AppCompatActivity {
     private void initData() {
 
         userList = new ArrayList<>();
-        userList.add(new ModelClass("ABC","Jamshoro","---------------------------------------------------------------------------------------------------",R.drawable.ic_person));
-        userList.add(new ModelClass("ABC","Jamshoro","---------------------------------------------------------------------------------------------------",R.drawable.ic_person));
-        userList.add(new ModelClass("ABC","Jamshoro","---------------------------------------------------------------------------------------------------",R.drawable.ic_person));
-        userList.add(new ModelClass("ABC","Jamshoro","---------------------------------------------------------------------------------------------------",R.drawable.ic_person));
-        userList.add(new ModelClass("ABC","Jamshoro","---------------------------------------------------------------------------------------------------",R.drawable.ic_person));
-        userList.add(new ModelClass("ABC","Jamshoro","---------------------------------------------------------------------------------------------------",R.drawable.ic_person));
-        userList.add(new ModelClass("ABC","Jamshoro","---------------------------------------------------------------------------------------------------",R.drawable.ic_person));
-        userList.add(new ModelClass("ABC","Jamshoro","---------------------------------------------------------------------------------------------------",R.drawable.ic_person));
-        userList.add(new ModelClass("ABC","Jamshoro","---------------------------------------------------------------------------------------------------",R.drawable.ic_person));
-        userList.add(new ModelClass("ABC","Jamshoro","---------------------------------------------------------------------------------------------------",R.drawable.ic_person));
-        userList.add(new ModelClass("ABC","Jamshoro","---------------------------------------------------------------------------------------------------",R.drawable.ic_person));
-        userList.add(new ModelClass("ABC","Jamshoro","---------------------------------------------------------------------------------------------------",R.drawable.ic_person));
+        userList.add(new ModelClass("Muhammad Faizan","Jamshoro","---------------------------------------------------------------------------------------------------",R.drawable.images));
+        userList.add(new ModelClass("Hamza Khan","Kotri","---------------------------------------------------------------------------------------------------",R.drawable.images1));
+        userList.add(new ModelClass("Huzafa Ali","Sehwan","---------------------------------------------------------------------------------------------------",R.drawable.images2));
+        userList.add(new ModelClass("Imdad Ali","Dadu","---------------------------------------------------------------------------------------------------",R.drawable.images3));
+        userList.add(new ModelClass("Asghar Ali","Hyderabad","---------------------------------------------------------------------------------------------------",R.drawable.images4));
+        userList.add(new ModelClass("Fayaz Khan","Thano Bola Khan","---------------------------------------------------------------------------------------------------",R.drawable.images5));
+        userList.add(new ModelClass("Asad Jamali","Bhan","---------------------------------------------------------------------------------------------------",R.drawable.images6));
+        userList.add(new ModelClass("Razaque ","Kotri","---------------------------------------------------------------------------------------------------",R.drawable.images7));
+        userList.add(new ModelClass("Faizan Ahmed","Hyderabad","---------------------------------------------------------------------------------------------------",R.drawable.images8));
+        userList.add(new ModelClass("Madad Ali","Jamshoro","---------------------------------------------------------------------------------------------------",R.drawable.images9));
+        userList.add(new ModelClass("Hyder Khan","Dadu","---------------------------------------------------------------------------------------------------",R.drawable.images10));
+        userList.add(new ModelClass("Hamza Ali","Kotri","---------------------------------------------------------------------------------------------------",R.drawable.images11));
     }
 
     private void initRecyclerView() {
